0788a76f003ae04d3b50cbd0233dde1afe5a1a23
Subrat Panda <subrpan@amazon.com>
2018-06-06 22:48:05 +0530

    Added code-push

--- ConsumablesHackathon2018App/AppWrapper.js
+++ ConsumablesHackathon2018App/AppWrapper.js
@@ -5,7 +5,12 @@ import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
 import App from './App';
 import { WelcomePage } from './js/ui/WelcomePage';
+import codePush from "react-native-code-push";
 
-export const AppWrapper = createStackNavigator({
+const AppWrapper = createStackNavigator({
   HomePage: { screen: WelcomePage },
 });
+
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, installMode: codePush.InstallMode.IMMEDIATE };
+AppWrapper = codePush(codePushOptions)(AppWrapper);
+export {AppWrapper}
