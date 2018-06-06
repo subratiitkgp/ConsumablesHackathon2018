33b077088e04f5ca2dcb4ba538e32f10e5bdf23d
Subrat Panda <subrpan@amazon.com>
2018-06-07 00:31:10 +0530

    Some productization

--- ConsumablesHackathon2018App/js/ui/WelcomePage.js
+++ ConsumablesHackathon2018App/js/ui/WelcomePage.js
@@ -8,7 +8,7 @@ import { RNCamera } from 'react-native-camera';
 
 export class WelcomePage extends Component {
   static navigationOptions = {
-    title: "Welcome"
+    title: "Grocery Shopping In Seconds"
   };
 
   constructor(props) {
@@ -18,6 +18,7 @@ export class WelcomePage extends Component {
     this.state = { asins };
     this.asinCount = asins.length + 1;
     this.scanProcessing = 0;
+    this.startTime = new Date();
     this.imageUrls = [
       "https://images-eu.ssl-images-amazon.com/images/I/51Kt7nFLqEL._SS140_.jpg",
       "https://images-eu.ssl-images-amazon.com/images/I/51dsI-aMgsL._SS140_.jpg",
@@ -38,12 +39,15 @@ export class WelcomePage extends Component {
     this.setState({asins: AsinStore.getAllAsins()});
     this.asinCount = 1;
     this.scanProcessing = 0;
+    this.startTime = new Date();
+    // this.printAsins();
   }
 
   addAsin() {
     AsinStore.saveAsin(this.getAsin("ASIN" + this.asinCount));
     this.setState({asins: AsinStore.getAllAsins()});
     this.asinCount = this.asinCount + 1;
+    // this.printAsins();
   }
 
   getAsin(id) {
@@ -57,8 +61,8 @@ export class WelcomePage extends Component {
   onBarCodeRead(data, type) {
     if (this.scanProcessing === 1) return;
     this.scanProcessing = 1;
-    console.log(data);
-    console.log(type);
+    // console.log(data);
+    // console.log(type);
     Alert.alert("Scanned code: ", 
                 data.data, 
                 [{text: 'Accept', onPress: () => {
@@ -76,16 +80,16 @@ export class WelcomePage extends Component {
     const { navigate } = this.props.navigation;
     return (
       <View style={{flex: 1}}>
-        {this.renderButtons()}
-        {this.renderAsinList()}
         {this.renderCamera()}
+        {this.renderAsinList()}
+        {this.renderButtons()}
       </View>
     )
   }
 
   renderCamera() {
     return (
-      <View style={{margin: 10, borderWidth: 1, width: "95%", height: 250}}>
+      <View style={{margin: 5, borderWidth: 1, width: "97%", height: 150}}>
         <RNCamera
           style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}
           type={RNCamera.Constants.Type.back}
@@ -100,23 +104,33 @@ export class WelcomePage extends Component {
 
   renderButtons() {
     return (
-      <View style={{margin: 10, width: '95%', flexDirection: 'row', borderWidth: 1, justifyContent: "space-evenly"}}>
-      <View style={{margin: 10}}>
+      <View style={{margin: 5, width: '97%', flexDirection: 'row', borderWidth: 1, justifyContent: "space-evenly"}}>
+      <View style={{margin: 5}}>
         <Button
           title="Add"
+          onPress={() => this.addAsin()}
+        />
+      </View>
+      <View style={{margin: 5}}>
+        <Button
+          title="Checkout"
           onPress={() => {
-            this.addAsin();
-            this.printAsins();
+            if (this.state.asins.length == 0) {
+              Alert.alert("Warning", "Please add few products to checkout");
+              return;
+            }
+            const endTime = new Date();
+            const timeDiff = (endTime - this.startTime) / 1000;
+            Alert.alert("Congrats", "You have shopped for " + this.state.asins.length + " item(s) in " + timeDiff + " seconds",
+                        [{text: "OK", onPress: () => this.deleteAllAsins()}]
+                       );
           }}
         />
       </View>
-      <View style={{margin: 10}}>
+      <View style={{margin: 5}}>
         <Button
           title="Reset"
-          onPress={() => {
-            this.deleteAllAsins();
-            this.printAsins();
-          }}
+          onPress={() => this.deleteAllAsins()}
         />
       </View>
     </View>
@@ -125,7 +139,7 @@ export class WelcomePage extends Component {
 
   renderAsinList() {
     return (
-      <View style={{flex: 1, margin : 10, borderWidth: 1}}>
+      <View style={{flex: 1, width: "97%", margin : 5, borderWidth: 1}}>
         <FlatList
           removeClippedSubviews={true}
           data={this.state.asins.reverse()}
@@ -142,8 +156,8 @@ export class WelcomePage extends Component {
     const asinId = asinSize - 1 - index;
     const imageUri = this.imageUrls[asinId % this.imageUrls.length];
     return (
-      <View style={{flexDirection: 'row', width: '95%', borderWidth: 1, margin: 5, alignItems: 'center', justifyContent: "flex-start"}}>
-        <Image source={{uri: imageUri}} style={{width: 30 , height: 40, marginRight: 50}} />
+      <View style={{flexDirection: 'row', width: '97%', borderWidth: 1, margin: 5, alignItems: 'center', justifyContent: "flex-start"}}>
+        <Image source={{uri: imageUri}} style={{width: 30 , height: 40, margin: 2, marginRight: 50}} />
         <Text style={{fontSize: 30}}>{asin.id}</Text>
       </View>
     )
