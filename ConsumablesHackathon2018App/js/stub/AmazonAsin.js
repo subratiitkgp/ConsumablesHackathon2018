51033812de50fcdf3fc0b17c8bc8646c2763a166
VaraKalyan <kalyanv@amazon.com>
2018-07-12 15:14:14 +0530

    Adding the data files

---
+++ ConsumablesHackathon2018App/js/stub/AmazonAsin.js
@@ -0,0 +1,126 @@
+export const AmazonAsinList = [
+    {
+        asin:"B01CJVVLUA",
+        imageURL:"https://images-eu.ssl-images-amazon.com/images/I/51dsI-aMgsL._SS125_.jpg",
+        available:"100",
+        title:"Surf Excel Matic Front Load Detergent Powder - 2 kg",
+        price:"314.00",
+        asingroup:"detergent",
+        type:"amazon",
+        variationgroup: "SurfExcel",
+        variation:"2 kg",
+        externalBarcode: "ext1"
+    },
+    {
+        asin:"B01CJVVUY2",
+        imageURL:"https://images-na.ssl-images-amazon.com/images/I/71PcniM-APL._AC_UL320_SR320,320_.jpg",
+        available:"100",
+        title:"Surf Excel Matic Front Load Detergent Powder - 1 kg",
+        price:"225.00",
+        asingroup:"detergent",
+        type:"amazon",
+        variationgroup: "SurfExcel",
+        variation:"1 kg",
+        externalBarcode: "ext2"
+    },
+    {
+        asin:"B01N5LENPM",
+        imageURL:"https://images-eu.ssl-images-amazon.com/images/I/513baVsqxfL._AC_UL246_SR190,246_FMwebp_QL70_.jpg",
+        available:"0",
+        title:"Ariel Matic Top Load Detergent Washing Powder - 2 kg",
+        price:"323.00",
+        asingroup:"detergent",
+        type:"amazon",
+        variationgroup: "Ariel",
+        variation:"2 kg",
+        externalBarcode: "ext3"
+    },
+    {
+        asin:"B01N5LENPM",
+        imageURL:"https://images-eu.ssl-images-amazon.com/images/I/51WngspBz4L._AC_UL648_SR500,648_FMwebp_QL65_.jpg",
+        available:"100",
+        title:"Ariel Matic Top Load Detergent Washing Powder - 6 kg",
+        price:"853.00",
+        asingroup:"detergent",
+        type:"amazon",
+        variationgroup: "Ariel",
+        variation:"6 kg",
+        externalBarcode: "ext4"
+    },
+    {
+        asin:"B01CHUR9RM",
+        imageURL:"https://images-eu.ssl-images-amazon.com/images/I/41XJB2v3lGL._AC_UL648_SR500,648_FMwebp_QL65_.jpg",
+        available:"100",
+        title:"Britannia Bourbon, 120g",
+        price:"18.00",
+        asingroup:"biscuit",
+        type:"amazon",
+        variationgroup: "bourbon",
+        variation:"120 g",
+        externalBarcode: "ext5"
+
+    },
+    {
+        asin:"B01CHUR9RM",
+        imageURL:"https://images-eu.ssl-images-amazon.com/images/I/416S-xou02L._AC_UL648_SR500,648_FMwebp_QL65_.jpg",
+        available:"100",
+        title:"Britannia Good Day Cashew, 200g",
+        price:"30.00",
+        asingroup:"biscuit",
+        type:"amazon",
+        variationgroup: "goodday",
+        variation:"200 g",
+        externalBarcode: "ext6"
+    },
+    {
+        asin:"B01M66ZABE",
+        imageURL:"https://images-eu.ssl-images-amazon.com/images/I/41GfcRfaQdL._AC_UL648_SR500,648_FMwebp_QL65_.jpg",
+        available:"100",
+        title:"Britannia Good Day Cashew, 100g",
+        price:"19.00",
+        asingroup:"biscuit",
+        type:"amazon",
+        variationgroup: "goodday",
+        variation:"100 g",
+        externalBarcode: "ext7"
+    },
+    {
+        asin:"B01CHUSV38",
+        imageURL:"https://images-eu.ssl-images-amazon.com/images/I/4145sK9RzJL._AC_UL648_SR500,648_FMwebp_QL65_.jpg",
+        available:"100",
+        title:"Sunfeast Mom's Magic Cashew and Almonds, 200 g",
+        price:"28.00",
+        asingroup:"biscuit",
+        type:"amazon",
+        variationgroup: "mom",
+        variation:"200 g",
+        externalBarcode: "ext7"
+    },
+    {
+        asin:"B01GCEGCP4",
+        imageURL:"https://images-eu.ssl-images-amazon.com/images/I/512AKf2zeFL._AC_UL587_SR453,587_FMwebp_QL65_.jpg",
+        available:"100",
+        title:"Madhur Pure and Hygienic Sugar, 1kg Bag",
+        price:"40.00",
+        asingroup:"sugar",
+        type:"amazon",
+        variationgroup: "madhur",
+        variation:"1 kg",
+        externalBarcode: "ext8"
+    },
+    {
+        asin:"B01GCF0XEY",
+        imageURL:"https://images-eu.ssl-images-amazon.com/images/I/51Kt7nFLqEL._AC_UL492_SR380,492_FMwebp_QL65_.jpg",
+        available:"100",
+        title:"Madhur Pure Sugar, 5kg Bag",
+        price:"195.00",
+        asingroup:"sugar",
+        type:"amazon",
+        variationgroup: "madhur",
+        variation:"5 kg",
+        externalBarcode: "ext9"
+    }
+
+
+    
+]
