6b9c8af22e8f9c9353b96ac0a78f6e28118e3ed9
Subrat Panda <subrpan@amazon.com>
2018-07-12 15:08:36 +0530

    Adding slider

--- ConsumablesHackathon2018App/js/ui/components/QuantitySlider.js
+++ ConsumablesHackathon2018App/js/ui/components/QuantitySlider.js
@@ -1,25 +1,47 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { Alert, View, Button, FlatList, Text, Image, TouchableHighlight } from 'react-native';
+import { Alert, View, Button, FlatList, Text, Image, TouchableWithoutFeedback } from 'react-native';
 
 export class QuantitySlider extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      quantity: 1
+    };
+  }
+
+  onPlusClick() {
+    this.setState({quantity: this.state.quantity + 1});
+    if (this.props.onQuantityChange != undefined) {
+      this.props.onQuantityChange(this.state.quantity);
+    }
+  }
+
+  onMinusClick() {
+    this.setState({quantity: this.state.quantity > 0 ? this.state.quantity - 1 : 0})
+    if (this.props.onQuantityChange != undefined) {
+      this.props.onQuantityChange(this.state.quantity);
+    }
   }
 
   render() {
     const plusImageSource = "../../images/PlusIcon.jpg";
     const minusImageSource = "../../images/MinusIcon.jpg";
 
-
     return (
-      <View style={{flex: 1}}>
-        <Text>Hello</Text>
-        <TouchableHighlight>
-        <Image source={require(plusImageSource)} style={{width: 40 , height: 40, margin: 2, marginRight: 50}} />
-        <Image source={require(minusImageSource)} style={{width: 40 , height: 40, margin: 2, marginRight: 50}} />
-        </TouchableHighlight>
+      <View style={{width: 150, height: 50, borderWidth: 0.5,
+                    flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+        <TouchableWithoutFeedback onPress={() => this.onPlusClick()}>
+          <Image source={require(plusImageSource)} style={{width: 50 , height: 50}} />
+        </TouchableWithoutFeedback>
+        <View style={{width: 50, height: 50, borderWidth: 0.5, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{fontSize: 30}}>{this.state.quantity}</Text>
+        </View>
+        <TouchableWithoutFeedback onPress={() => this.onMinusClick()}>
+          <Image source={require(minusImageSource)} style={{width: 50 , height: 50}} />
+        </TouchableWithoutFeedback>
       </View>
     )
   }
