b79cc09e578648a9e9ddb40cd0245f08fa998e9a
Subrat Panda <subrpan@amazon.com>
2018-07-12 15:11:01 +0530

    Adding slider

--- ConsumablesHackathon2018App/AppWrapper.js
+++ ConsumablesHackathon2018App/AppWrapper.js
@@ -3,16 +3,12 @@
 import React, { Component } from 'react';
 
 import { createStackNavigator } from 'react-navigation';
-import codePush from "react-native-code-push";
-
 import App from './App';
 import { WelcomePage } from './js/ui/WelcomePage';
-import { CartPage } from './js/ui/pages/CartPage';
-import { QuantitySlider } from './js/ui/components/QuantitySlider';
+import codePush from "react-native-code-push";
 
 const AppWrapper = createStackNavigator({
-  HomePage: { screen: QuantitySlider },
-  CartPage: { screen: CartPage }
+  HomePage: { screen: WelcomePage },
 });
 
 let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, installMode: codePush.InstallMode.IMMEDIATE };
